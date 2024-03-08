ac0c530861965fe05cc783f005fa629d0d010719
Damien Robinson <damien.robinson@pocketpasta.com>
2024-03-08 14:54:03 +1000

    chore(home page): add heading

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,15 @@
 <template>
-  <div>Test</div>
+  <div>
+    <section class="container">
+      <div class="row">
+        <div class="col">
+          <h1 class="text-4xl font-extrabold dark:text-white">Links</h1>
+        </div>
+      </div>
+    </section>
+    
+    <section class="container">
+      A bookmark manager.
+    </section>
+  </div>
 </template>
