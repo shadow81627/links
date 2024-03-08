bcf2352bbf4927b71d722276b3bfffb09dddf792
Damien Robinson <damien.robinson@pocketpasta.com>
2024-03-08 14:47:23 +1000

    chore(deps): add prettier and eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: ["@nuxt/eslint-config", "plugin:prettier/recommended"],
+  plugins: ["prettier"],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "vue/html-self-closing": "off",
+  },
+};
