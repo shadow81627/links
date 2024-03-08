8a294f274efb6a0010a6b4bcffdd491267351d7b
Damien Robinson <damien.robinson@pocketpasta.com>
2024-03-08 15:35:23 +1000

    chore: add head meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,4 +23,24 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@unocss/nuxt", "nuxt-icon", "nuxt-lodash"],
   css: ["@unocss/reset/tailwind.css"],
+
+  app: {
+    head: {
+      title: "Mithaka Seed Key",
+      viewport: "width=device-width,initial-scale=1",
+      link: [
+        { rel: "icon", href: "/favicon.ico", sizes: "any" },
+        { rel: "apple-touch-icon", href: "/apple-touch-icon-180x-180.png" },
+        // { rel: "manifest", href: "/manifest.webmanifest" },
+      ],
+      meta: [
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { name: "description", content: "Mithaka Seed Key" },
+        {
+          name: "apple-mobile-web-app-status-bar-style",
+          content: "black-translucent",
+        },
+      ],
+    },
+  },
 });
