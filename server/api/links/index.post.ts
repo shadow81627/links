ecf7b999fbacc70f76b8b1ac311007b246d6ea29
Damien Robinson <damien.robinson@pocketpasta.com>
2024-08-18 19:21:04 +1000

    feat(api links): add validation

--- server/api/links/index.post.ts
+++ server/api/links/index.post.ts
@@ -1,5 +1,8 @@
 import { Link } from "~/server/database/models/link";
 // import { sql } from "drizzle-orm";
+import { createInsertSchema } from "drizzle-valibot";
+import { links } from "~/server/database/schema/links";
+import { parse, pipe, safeParse, string, url } from "valibot";
 
 export default defineEventHandler(async (event) => {
   const user = event.context.user;
@@ -11,9 +14,31 @@ export default defineEventHandler(async (event) => {
   }
   // https://www.nexusmods.com/stardewvalley/mods/3040
 
+  const schema = createInsertSchema(links, {});
+
   const formData = await readFormData(event);
-  const url = new URL(Object.fromEntries(event.node.req.body)?.url ?? "");
-  const link = Link.firstOrCreate(url);
+  const validation = parse(schema, Object.fromEntries(formData));
+  if (!validation) {
+    throw createError({
+      message: "Unprocessable Content",
+      statusCode: 422,
+      data: validation,
+    });
+  }
+
+  const UrlSchema = pipe(
+    string("A URL must be string."),
+    url("The URL is badly formatted."),
+  );
+  const urlValidation = safeParse(UrlSchema, Object.fromEntries(formData)?.url);
+  if (!urlValidation.success) {
+    throw createError({
+      message: "Unprocessable Content",
+      statusCode: 422,
+      data: validation,
+    });
+  }
+  const link = await Link.firstOrCreate(new URL(urlValidation.output ?? ""));
 
   // const values = await db.query.links.findMany({
   //   extras: {
@@ -23,7 +48,7 @@ export default defineEventHandler(async (event) => {
   //   },
   // });
   const data = {
-    id: link.id,
+    id: link?.id,
     type: "links",
     attributes: { ...link },
   };
