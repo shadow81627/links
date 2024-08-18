866ecc54fa28acb10233fe15aa8100b744c39344
Damien Robinson <damien.robinson@pocketpasta.com>
2024-08-18 18:59:26 +1000

    fix(contents schema): add default id

--- server/database/schema/contents.ts
+++ server/database/schema/contents.ts
@@ -2,9 +2,12 @@ import { sql, relations } from "drizzle-orm";
 import { sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { files } from "./files";
 import { links } from "./links";
+import { ulid } from "ulidx";
 
 export const contents = sqliteTable("contents", {
-  id: text("id").primaryKey(),
+  id: text("id")
+    .primaryKey()
+    .$default(() => ulid()),
   slug: text("slug"),
   name: text("name"),
   description: text("description"),
